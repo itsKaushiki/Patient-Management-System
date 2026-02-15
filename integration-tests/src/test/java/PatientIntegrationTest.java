dac7b3be607942332dd6ab7b59562e6c72fffc48
itsKaushiki <2022ugec032@nitjsr.ac.in>
2026-02-16 00:50:04 +0530

    Project successfully running locally with Java21 + Colima, fixed gRPC protobuf and service ports

--- integration-tests/src/test/java/PatientIntegrationTest.java
+++ integration-tests/src/test/java/PatientIntegrationTest.java
@@ -38,6 +38,6 @@ public class PatientIntegrationTest {
         .get("/api/patients")
         .then()
         .statusCode(200)
-        .body("patients", notNullValue());
+        .body(notNullValue());
   }
 }
